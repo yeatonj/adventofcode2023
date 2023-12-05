0b3e729db5146661292a3794b2534fa238ad5a99
Josh Yeaton <yeaton.josh@gmail.com>
2023-12-05 08:59:15 -0500

    Completed day 4

--- day4/day4.py
+++ day4/day4.py
@@ -1,13 +1,24 @@
 # File for day 4 of AoC 2023
 # Written by Joshua Yeaton on 12/4/2023
 
-f = open('data.txt')
-# f = open('data-test.txt')
+fname = 'data.txt'
+# fname = 'data-test.txt'
+f = open(fname)
 
+# create array for the number of cards
+card_count = []
+for line in f:
+    card_count.append(1)
+
+f.close()
+
+# Reopen the file and calculate total points
+f = open(fname)
 total_points = 0
-
+cur_card = 0
 for line in f:
     line_points = 0
+    line_win_count = 0
     entries = line.split('|')
     # First, extract the winning numbers
     winning_nums = ''.join(entries[0])
@@ -25,11 +36,21 @@ for line in f:
     # now calculate the points
     for guess in selected_nums:
         if ((guess in winning_nums) and (line_points == 0)):
+            line_win_count += 1
             line_points = 1
         elif (guess in winning_nums):
+            line_win_count += 1
             line_points *= 2
+    # now, add the copies of cards we've won to the total number of cards we have
+    i = cur_card + 1
+    while ((i < len(card_count)) and (line_win_count > 0)):
+        card_count[i] += card_count[cur_card]
+        line_win_count -= 1
+        i += 1
     total_points += line_points
+    cur_card += 1
 
 print('Total points: ' + str(total_points))
+print('Total cards: ' + str(sum(card_count)))
 
 f.close()
