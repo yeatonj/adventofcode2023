c32591b3068f1ab8d9a20681f01121187e91d0f1
Josh Yeaton <yeaton.josh@gmail.com>
2023-12-11 15:53:03 -0500

    Completed day 11

--- day11/day11.py
+++ day11/day11.py
@@ -2,57 +2,67 @@
 # Written by Joshua Yeaton on 12/11/2023
 
 file_name = 'data.txt'
-file_name = 'data-test.txt'
+# file_name = 'data-test.txt'
 
 f = open(file_name)
 
 expanded_galaxy = []
 
-# Find rows with no galaxies
+# Find rows with no galaxies and coords of galaxies
+row = 0
+empty_rows = []
+galaxies = []
 for line in f:
     line = line.strip()
     found_gal = False
     expanded_galaxy.append(list(line))
+    col = 0
     for c in line:
         if c == '#':
             found_gal = True
-            break
-#     if not found_gal:
-#         # Insert the galaxy
-#         expanded_galaxy.append(list(line))
-    
-# # Find columns with no galaxies
-# insert_cols = []
-# for i in range(len(line)):
-#     col = [row[i] for row in expanded_galaxy]
-#     found_gal = False
-#     for c in col:
-#         if c == '#':
-#             found_gal = True
-#     if not found_gal:
-#         insert_cols.append(i)
+            galaxies.append((row,col))
+        col += 1
+    if not found_gal:
+        empty_rows.append(row)
+    row += 1
+empty_rows.sort(reverse=True)
 
-# # Now, add those columns
-# insert_cols.sort(reverse=True)
-# for line in expanded_galaxy:
-#     for col in insert_cols:
-#         line.insert(col, '.')
+# Find columns with no galaxies
+empty_cols = []
+for i in range(len(line)):
+    col = [row[i] for row in expanded_galaxy]
+    found_gal = False
+    for c in col:
+        if c == '#':
+            found_gal = True
+    if not found_gal:
+        empty_cols.append(i)
+empty_cols.sort(reverse=True)
 
-# Now, save the coordinates of each galaxy into a list
-rows = len(expanded_galaxy)
-cols = len(expanded_galaxy[0])
-galaxies = []
-for r in range(rows):
-    for c in range(cols):
-        if expanded_galaxy[r][c] == '#':
-            galaxies.append((r,c))
+# Now, find the actual galaxy coordinates
+# dist = 2
+dist = 1000000
+actual_gals = []
+for g in galaxies:
+    addl_rows = 0
+    for r in empty_rows:
+        if g[0] > r:
+            addl_rows += 1
+    addl_cols = 0
+    for c in empty_cols:
+        if g[1] > c:
+            addl_cols += 1
+    actual_rows = addl_rows * (dist - 1) + g[0]
+    actual_cols = addl_cols * (dist - 1) + g[1]
+    actual_gals.append((actual_rows, actual_cols))
 
+# find our distances
 total_dist = 0
-total_gals = len(galaxies)
+total_gals = len(actual_gals)
 for gal_1 in range(total_gals):
     for gal_2 in range(gal_1 + 1, total_gals):
-        total_dist += (abs(galaxies[gal_1][0] - galaxies[gal_2][0]) + abs(galaxies[gal_1][1] - galaxies[gal_2][1]))
+        total_dist += (abs(actual_gals[gal_1][0] - actual_gals[gal_2][0]) + abs(actual_gals[gal_1][1] - actual_gals[gal_2][1]))
 
-# Part 1 solution
+# solution
 print(total_dist)
 
