c0f8091bafd7fee3151afb52f17d8b8c5890ec84
Josh Yeaton <yeaton.josh@gmail.com>
2024-01-03 18:48:32 -0500

    Added templates

---
+++ day25/day25.py
@@ -0,0 +1,10 @@
+# File for day 25 of AoC 2023
+# Written by Joshua Yeaton on 1/3/2024
+
+if __name__ == '__main__':
+    file_name = 'data.txt'
+    file_name = 'data-test.txt'
+
+    f = open(file_name)
+
+    f.close()
