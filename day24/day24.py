0bc08cb0557b2953b298c0e99d0290f7d4f5030d
Josh Yeaton <yeaton.josh@gmail.com>
2024-01-05 15:08:53 -0500

    Completed day 24

--- day24/day24.py
+++ day24/day24.py
@@ -1,6 +1,8 @@
 # File for day 24 of AoC 2023
 # Written by Joshua Yeaton on 1/3/2024
 
+import numpy
+
 # hailstone data is an array with entries [([pos0], [vel0]), ..., ([posn], [veln])]
 def parse_input(file_name):
     f = open(file_name)
@@ -51,6 +53,91 @@ def check_intersection(eq1, eq2, dim_x, dim_y, init_x_pos1, x_vel1, init_x_pos2,
             return True
     return False
 
+def solve_pt_2(hailstone_data):
+    # pick 3 hailstones
+    stone_1 = hailstone_data[0]
+    stone_2 = hailstone_data[1]
+    stone_3 = hailstone_data[2]
+
+    x0 = stone_1[0][0]
+    x1 = stone_2[0][0]
+    x2 = stone_3[0][0]
+
+    y0 = stone_1[0][1]
+    y1 = stone_2[0][1]
+    y2 = stone_3[0][1]
+
+    z0 = stone_1[0][2]
+    z1 = stone_2[0][2]
+    z2 = stone_3[0][2]
+
+    dx0 = stone_1[1][0]
+    dx1 = stone_2[1][0]
+    dx2 = stone_3[1][0]
+
+    dy0 = stone_1[1][1]
+    dy1 = stone_2[1][1]
+    dy2 = stone_3[1][1]
+
+    dz0 = stone_1[1][2]
+    dz1 = stone_2[1][2]
+    dz2 = stone_3[1][2]
+
+    # we need a 6 x 6 matrix with appropriate coeff
+    # use vector [X, Y, Z, dX, dY, dZ]
+    a = numpy.zeros((6, 6))
+    b = numpy.zeros(6)
+    # eqn 1
+    a[0][0] = dy1 - dy0
+    a[0][1] = dx0 - dx1
+    a[0][2] = 0
+    a[0][3] = y0 - y1
+    a[0][4] = x1 - x0
+    a[0][5] = 0
+    b[0] = x1*dy1 - y1*dx1 - x0*dy0 + y0*dx0
+    # eqn 2
+    a[1][0] = dz1 - dz0
+    a[1][1] = 0
+    a[1][2] = dx0 - dx1
+    a[1][3] = z0 -z1
+    a[1][4] = 0
+    a[1][5] = x1 - x0
+    b[1] = x1*dz1 - z1*dx1 - x0*dz0 +z0*dx0
+    # eqn 3
+    a[2][0] = 0
+    a[2][1] = dz0 - dz1
+    a[2][2] = dy1 - dy0
+    a[2][3] = 0
+    a[2][4] = z1 - z0
+    a[2][5] = y0 - y1
+    b[2] = z1*dy1 - y1*dz1 - z0*dy0 + y0*dz0
+    # eqn 4
+    a[3][0] = dy2 - dy0
+    a[3][1] = dx0 - dx2
+    a[3][2] = 0
+    a[3][3] = y0 - y2
+    a[3][4] = x2 - x0
+    a[3][5] = 0
+    b[3] = x2*dy2 - y2*dx2 - x0*dy0 + y0*dx0
+    # eqn 5
+    a[4][0] = dz2 - dz0
+    a[4][1] = 0
+    a[4][2] = dx0 - dx2
+    a[4][3] = z0 -z2
+    a[4][4] = 0
+    a[4][5] = x2 - x0
+    b[4] = x2*dz2 - z2*dx2 - x0*dz0 +z0*dx0
+    # eqn 6
+    a[5][0] = 0
+    a[5][1] = dz0 - dz2
+    a[5][2] = dy2 - dy0
+    a[5][3] = 0
+    a[5][4] = z2 - z0
+    a[5][5] = y0 - y2
+    b[5] = z2*dy2 - y2*dz2 - z0*dy0 + y0*dz0
+    
+    soln = numpy.linalg.solve(a,b)
+    return sum(soln[0:3])
 
 
 
@@ -67,4 +154,7 @@ if __name__ == '__main__':
 
     ans_pt_1 = check_intersections(eqns_pt_1, test_dim, test_dim, hailstone_data)
 
+    ans_pt_2 = solve_pt_2(hailstone_data)
+
     print(ans_pt_1)
+    print(ans_pt_2)
